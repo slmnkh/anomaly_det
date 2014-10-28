58de7654297178c28c331f850eb1030a44f0ce3c
slmnkh <slmnkhokhar@gmail.com>
2014-10-28 19:19:03 -0400

    Adding docs

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["Welcome to anomaly_detection&#8217;s documentation!"],objects:{},titleterms:{anomaly_detect:0,document:0,welcom:0,indic:0,tabl:0}})
